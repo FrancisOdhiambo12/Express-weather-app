c1e7a1b7aea80f50d4d577a288484122e76259c1
Francis <francisodhiambo2@gmail.com>
2023-04-20 01:58:12 +0300

    Getting data using get request

--- app.js
+++ app.js
@@ -22,7 +22,17 @@ app.get("/", function(req, res){
    https.get(url, function(response){
     console.log(response.statusCode);
 
-   })
+    response.on("data", function(data) {
+        const weatherData = JSON.parse(data)
+        const temp = weatherData.main.temp
+        const weatherDescription = weatherData.weather[0].description
+        const icon = weatherData.weather[0].icon
+        const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
+        res.write("<p>The weather in Nairobi is " + weatherDescription + ".</p>")
+        res.write("<h1>The temperature in Nairobi is " + temp + " degrees celcius.</h1>")
+        res.write("<img src=" + imageURL + "/>")
+        res.send()
 
-   res.send("Server is up and running")
+    })
+   })
 })
